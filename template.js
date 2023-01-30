cf0a66a229382011e3bae7f9e2f94f855971cd0e
hamidr-source <118419158+hamidr-source@users.noreply.github.com>
2023-01-30 19:32:19 +0330

    Create template.js

---
+++ template.js
@@ -0,0 +1,10 @@
+const template = "h: m: s: t";
+
+const template = "h: m: s: t"
+const clock = template
+    .replace("h", "03")
+    .replace("m", "33")
+    .replace("s", "33")
+    .replace("t", "AM")
+
+console.log(clock)
